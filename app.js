8cd50840a6c9266d55989368f9fa8ecf6e23e711
rramosx11 <rodolforamosd11@gmail.com>
2022-07-28 01:35:33 -0400

    completed the html structure and created src-page template

---
+++ app.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+// const printProfileData = (profileDataArr) => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log("================");
+
+//   // Is the same as this...
+//   profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+// const generatePage = (userName, githubName) =>
+//   `Name: ${userName}, Github: ${githubName}`;
+
+// recoding the const generatePage to create a multi-line input
+
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
