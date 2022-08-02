52c62292d1535f4b681bc169a5c38b1f3415165f
rramosx11 <rodolforamosd11@gmail.com>
2022-08-01 21:51:22 -0400

    added the updated html feature

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
 // const pageHTML = generatePage(name, github);
 
@@ -151,5 +151,11 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw err;
+      console.log(
+        "Portfolio complete! Check out index.html to see the output!"
+      );
+    });
   });
